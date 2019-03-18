9df7432fb87bc47680bae480c44c774fe142f3c7
unknown <jallorenzo7@gmail.com>
2019-03-19 03:16:12 +0800

    added question crude

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -5,17 +5,127 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Modules\User\Entities\User;
 use App\GroupQuestion;
-use App\GroupQuestion;
-use App\GroupQuestion;
-use App\GroupQuestion;
+use App\Question;
+use App\QuestionOption;
+use App\UserQuestion;
 
 class QuestionController extends Controller
 {
-	public function __construct()
-	{
+    public function __construct(
+        User $user_model,
+        GroupQuestion $group_question,
+        Question $question,
+        QuestionOption $question_option,
+        UserQuestion $user_question
+    ) {
+        $this->user_model      = $user_model;
+        $this->group_question  = $group_question;
+        $this->question        = $question;
+        $this->question_option = $question_option;
+        $this->user_question   = $user_question;
+    } 
 
-	} 
+    public function questionCreate(Request $request)
+    {
+        $data = $request->all();
+        $this->group_question->create($data);
+        return redirect()->back();
+    }
+
+    public function addQuestion(Request $request)
+    {
+        $data = $request->all();
+        $this->question->create($data);
+        return redirect()->back();
+    }
+
+    public function addQuestionOption(Request $request)
+    {
+        $data = $request->all();
+        $this->question_option->create($data);
+        return redirect()->back();
+    }
+
+    public function addUserQuestion(Request $request)
+    {
+        $data = $request->all();
+        $this->user_question->create($data);
+        return redirect()->back();
+    }
 
 
+    public function questionUpdate(Request $request, $id)
+    {
+        $data = $request->all();
+        $group_question = $this->group_question->find($data['id']);
+        if (empty($group_question)) {
+            return redirect()->back();
+        }
+        $group_question->fill($data);
+        $group_question->save();
+        return redirect()->back();
+    }
+
+    public function updateQuestion(Request $request)
+    {
+        $data = $request->all();
+
+        $question = $this->question->find($data['id']);
+        if (empty($question)) {
+            return redirect()->back();
+        }
+        $question->fill($data);
+        $question->save();
+
+        return redirect()->back();
+    }
+
+    public function updateQuestionOption(Request $request)
+    {
+        $data = $request->all();
+        $question_o = $this->question_option->find($data['id']);
+        if (empty($question)) {
+            return redirect()->back();
+        }
+        $question_o->fill($data);
+        $question_o->save();
+        return redirect()->back();
+    }
+
+
+    public function questionDelete(Request $request, $id)
+    {
+        $data = $request->all();
+        $group_question = $this->group_question->find($data['id']);
+        if (empty($group_question)) {
+            return redirect()->back();
+        }
+        $group_question->delete();
+        return redirect()->back();
+    }
+
+    public function deleteQuestion(Request $request)
+    {
+        $data = $request->all();
+
+        $question = $this->question->find($data['id']);
+        if (empty($question)) {
+            return redirect()->back();
+        }
+        $question->delete();
+
+        return redirect()->back();
+    }
+
+    public function deleteQuestionOption(Request $request)
+    {
+        $data = $request->all();
+        $question_o = $this->question_option->find($data['id']);
+        if (empty($question)) {
+            return redirect()->back();
+        }
+        $question_o->delete();
+        return redirect()->back();
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,56 @@ Route::group(['middleware' => ['web', 'auth']], function () {
         'as' => 'dashboard',
     ]);
 
+    Route::post('/question/group/create', [
+        'uses' => 'QuestionController@questionCreate',
+        'as' => 'question.group.create',
+    ]);
+
+    Route::post('/question/create', [
+        'uses' => 'QuestionController@addQuestion',
+        'as' => 'question.create',
+    ]);
+
+    Route::post('/question/option/create', [
+        'uses' => 'QuestionController@addQuestionOption',
+        'as' => 'question.option.create',
+    ]);
+
+    Route::post('/add/user/question', [
+        'uses' => 'QuestionController@addUserQuestion',
+        'as' => 'add.user.question',
+    ]);
+
+    Route::patch('/question/group/update', [
+        'uses' => 'QuestionController@questionUpdate',
+        'as' => 'question.group.update',
+    ]);
+
+    Route::patch('/question/Update', [
+        'uses' => 'QuestionController@updateQuestion',
+        'as' => 'question.update',
+    ]);
+
+    Route::patch('/question/option/Update', [
+        'uses' => 'QuestionController@updateQuestionOption',
+        'as' => 'question.option.update',
+    ]);
+
+    Route::delete('/question/group/delete', [
+        'uses' => 'QuestionController@questionDelete',
+        'as' => 'question.group.delete',
+    ]);
+
+    Route::delete('/question/delete', [
+        'uses' => 'QuestionController@deleteQuestion',
+        'as' => 'question.delete',
+    ]);
+
+    Route::delete('/question/option/delete', [
+        'uses' => 'QuestionController@deleteQuestionOption',
+        'as' => 'question.option.delete',
+    ]);
+
 });
 
 Auth::routes();
