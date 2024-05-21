2092fd59985670982503649f4b7c1a8dbc36ef80
BOSTON\bosto <arnoldboston1234567890@gmail.com>
2024-05-22 00:33:42 +0800

    fix

--- Modules/Core/Config/core.php
+++ Modules/Core/Config/core.php
@@ -9,7 +9,7 @@ return [
     'system_title' => 'Online Questionnaire',
     'system_title_abbreviation' => 'OQ',
 
-    'copyright' => '2019',
+    'copyright' => '2024',
 
     'password_generated' => 'oq' . '-' . mt_rand(5, 99999),
     'code_generator' =>  'OMNI' . mt_rand(5, 99999),
