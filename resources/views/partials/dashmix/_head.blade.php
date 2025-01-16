a232d385b9775be60c10fd4219fdd61f5050e3b7
bostonarnold19 <arnoldboston1234567890@gmail.com>
2025-01-16 09:43:22 +0800

    fix color

--- resources/views/partials/dashmix/_head.blade.php
+++ resources/views/partials/dashmix/_head.blade.php
@@ -17,4 +17,3 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
 @yield('styles')
 <link rel="stylesheet" id="css-main" href="{{ asset('themes/dashmix/assets/css/dashmix.min.css') }}">
-<link rel="stylesheet" id="css-main" href="{{ asset('themes/dashmix/assets/css/themes/xeco.css') }}">
