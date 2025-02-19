fffb07b85a236c02f41a85c9fe1e9be81d7e9c00
unknown <jallorenzo7@gmail.com>
2025-02-19 11:00:15 +0800

    added to fixed to exam mode

--- public/js/exam-mode.js
+++ public/js/exam-mode.js
@@ -239,7 +239,7 @@ const app = new Vue({
                             var grade = ((_this.score/_this.items) * 100) >= _this.passing;
                     var msg = 'Congrats';
                     var type = 'success';
-                    var ave = ((_this.score/_this.items) * 100) + "%";
+                    var ave = ((_this.score/_this.items) * 100).toFixed(2) + "%";
 
                     if(grade == false) {
                         msg = 'Failed';
